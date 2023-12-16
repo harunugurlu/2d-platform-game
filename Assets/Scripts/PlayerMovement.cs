526ba1eda07015140fa49a62df7fc16d2a01ac95
harunugurlu <harunugurlu1@gmail.com>
2023-12-16 15:06:17 +0300

    basic movement (will improve)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         Debug.Log("Hello World!");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        rb.gravityScale = 5.0f;
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && rb.velocity.y <= 0)
+        {
+            rb.velocity = new Vector2(0, 16f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        { 
+            rb.velocity = new Vector2(-8.0f, 4.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(8.0f, 4.0f);
+        }
     }
 }
