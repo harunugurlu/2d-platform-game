8f4d6ec6bb388935daa4cc7ccc9ffffe4bd52afa
harunugurlu <harunugurlu1@gmail.com>
2023-12-26 14:34:45 +0300

    add sprite renderer flipX

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //TODO: Add Layers to ground and walls, use LayerMasks to detect collision and add double jump. numJumps will be resetted to 2, when the player is on ground
+
     private Rigidbody2D rb;
 
     private BoxCollider2D boxCollider;
     
     private CompositeCollider2D compositeCollider;
+
+    private SpriteRenderer spriteRenderer;
 
     private Animator playerAnimator;
     
@@ -18,8 +22,21 @@ public class PlayerMovement : MonoBehaviour
     
     private GameObject terrain;
 
+    private int isRunningHash;
+    private int isJumpingHash;
+    private int isFallingHash;
+
+    private int numJumps = 2;
+    private float dirX = 0f;
+
     [SerializeField]
     private float smoothingFactor;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    [SerializeField]
+    private LayerMask wallLayer;
 
     // Start is called before the first frame update
     private void Start()
@@ -28,9 +45,15 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
         playerAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         //tilemapCollider = GetComponent<TilemapCollider2D>();
         terrain = GameObject.Find("Terrain");
         camera = GameObject.Find("Main Camera");
+
+        int isRunningHash = Animator.StringToHash("isRunning");
+        int isJumpingHash = Animator.StringToHash("isJumping");
+        int isFallingHash = Animator.StringToHash("isFalling");
+
         InitCompositeCollider();
         LockOnPlayer();
     }
@@ -38,9 +61,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
 
-        UpdatePlayerMovement(dirX);
+        UpdatePlayerMovement();
         Debug.Log("rb.velocity: " + rb.velocity.y);
     }
 
@@ -49,11 +72,28 @@ public class PlayerMovement : MonoBehaviour
         LockOnPlayer();
     }
 
-    private void UpdatePlayerMovement(float dirX)
+    private void HandlePlayerInpts()
     {
+
+    }
+
+    private void UpdatePlayerMovement()
+    {
+        // Cache the parameter IDs using Animator.StringToHash
+
+        float posX = rb.velocity.x;
+        float posY = rb.velocity.y;
 
         if (dirX != 0)
         {
+            if(dirX < 0)
+            {
+                SpritRendererFlipX(true);
+            }
+            else
+            {
+                SpritRendererFlipX(false);
+            }
             playerAnimator.SetBool("isRunning", true);
         }
         else
@@ -61,14 +101,12 @@ public class PlayerMovement : MonoBehaviour
             playerAnimator.SetBool("isRunning", false);
         }
 
-        float posX = rb.velocity.x;
-        float posY = rb.velocity.y;
-
         rb.velocity = new Vector2(dirX * 7f, posY);
 
-        if (Input.GetButtonDown("Jump") && boxCollider.IsTouching(compositeCollider))
+        if (Input.GetButtonDown("Jump") && (boxCollider.IsTouching(compositeCollider) || playerAnimator.GetBool("isFalling")) && numJumps > 0)
         {
             rb.velocity = new Vector2(posX, 16f);
+            numJumps--;
         }
         
         if(rb.velocity.y > 1)
@@ -92,6 +130,16 @@ public class PlayerMovement : MonoBehaviour
         {
             compositeCollider = terrain.GetComponent<CompositeCollider2D>();
         }
+    }
+
+    private bool CanJump()
+    {
+        return boxCollider.IsTouching(compositeCollider) || playerAnimator.GetBool(isFallingHash);
+    }
+
+    private void SpritRendererFlipX(bool flipX)
+    {
+        spriteRenderer.flipX = flipX;
     }
 
     private void LockOnPlayer()
